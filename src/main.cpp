9d3ccdf2eb7ff9bc97fd93c30bc4f86609cb818d
Bradford Bazemore <bankersalgo@gmail.com>
2019-01-20 16:32:46 -0500

    sleeping and watkong on button

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,14 @@
 #include <Arduino.h>
-#include <ArduinoLog.h>
 #include <AutoConnect.h>
 #include <WiFi.h>
 #include <WebServer.h>
 
 #ifdef DEBUG
-#define LOG_LEVEL LOG_LEVEL_VERBOSE
 #define AC_DEBUG
 #define AC_DEBUG_PORT Serial
 #endif
 
 #ifndef DEBUG
-#define LOG_LEVEL LOG_LEVEL_NOTICE
 #undef AC_DEBUG
 #endif
 
@@ -21,46 +18,61 @@
 
 #define CONFIG_PIN 27
 
-enum Mode
-{
-  Sleeping,
-  Diving,
-  Config
-};
+#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP 5        /* Time ESP32 will go to sleep (in seconds) */
 
 WebServer Server;
 AutoConnect Portal(Server);
 
-Mode mode = Sleeping;
-
-void printTimestamp(Print *_logOutput)
-{
-  char c[12];
-  sprintf(c, "%10lu ", millis());
-  _logOutput->print(c);
-}
-
-void checkButtons()
-{
-  if (digitalRead(CONFIG_PIN) == HIGH && mode == Sleeping)
-  {
-    mode = Config;
-    Log.notice("starting config portal\n");
-    AutoConnectConfig acConfig(AP_NAME, AP_PASS);
-    acConfig.autoReconnect = false;
-    acConfig.autoReset = true;
-    Portal.config(acConfig);
-    Portal.begin();
-  }
-}
 
 void runPortal()
 {
-  if (mode == Config && WiFi.getMode() == WIFI_AP_STA)
+  Serial.printf("starting config portal\n");
+  AutoConnectConfig acConfig(AP_NAME, AP_PASS);
+  acConfig.autoReconnect = false;
+  acConfig.autoReset = true;
+  Portal.config(acConfig);
+  Portal.begin();
+  while (WiFi.getMode() == WIFI_AP_STA)
   {
     Portal.handleClient();
   }
 }
+
+void wakeup()
+{
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+
+  switch (wakeup_reason)
+  {
+  case ESP_SLEEP_WAKEUP_EXT0:
+    Serial.println("Wakeup caused by external signal using RTC_IO");
+    runPortal();
+    break;
+  case ESP_SLEEP_WAKEUP_EXT1:
+    Serial.println("Wakeup caused by external signal using RTC_CNTL");
+    delay(1000);
+    break;
+  case ESP_SLEEP_WAKEUP_TIMER:
+    Serial.println("Wakeup caused by timer");
+    delay(1000);
+    break;
+  case ESP_SLEEP_WAKEUP_TOUCHPAD:
+    Serial.println("Wakeup caused by touchpad");
+    delay(1000);
+    break;
+  case ESP_SLEEP_WAKEUP_ULP:
+    Serial.println("Wakeup caused by ULP program");
+    delay(1000);
+    break;
+  default:
+    Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+    delay(1000);
+    break;
+  }
+}
+
 void setup()
 {
   Serial.begin(9600);
@@ -69,16 +81,15 @@ void setup()
 
   pinMode(CONFIG_PIN, INPUT);
 
-  Log.begin(LOG_LEVEL, &Serial);
-  Log.setPrefix(printTimestamp);
+  Serial.printf("system starting...\n");
+  Serial.printf("build Version: %D\n", BUILD_VERSION);
+  Serial.printf("built At: %d\n", BUILD_TIME);
 
-  Log.notice("system starting...\n");
-  Log.notice("build Version: %D\n", BUILD_VERSION);
-  Log.notice("built At: %d\n", BUILD_TIME);
+  wakeup();
+
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_27, 1);
+  Serial.println("Going to sleep now");
+  esp_deep_sleep_start();
 }
 
-void loop()
-{
-  checkButtons();
-  runPortal();
-}
+void loop() {}
