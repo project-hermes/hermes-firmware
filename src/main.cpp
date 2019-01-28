ed3e645a78984965fc8dca53b5ecd1345933414e
Bradford Bazemore <bankersalgo@gmail.com>
2019-01-27 19:45:56 -0500

    init

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <AutoConnect.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <Wire.h>
+#include "Adafruit_GPS.h"
 
 #ifdef DEBUG
 #define AC_DEBUG
@@ -16,9 +18,14 @@
 #define BUILD_VERSION 0.0
 #endif
 
+#define GPSSerial Serial1
+Adafruit_GPS GPS(&GPSSerial);
+#define GPSECHO false
+uint32_t timer = millis();
+
+
 WebServer Server;
 AutoConnect Portal(Server);
-
 
 String getMacAddress() {
 	uint8_t baseMac[6];
@@ -40,6 +47,14 @@ void runPortal()
   {
     Portal.handleClient();
   }
+}
+
+void startDive(){
+  
+}
+
+void readGPS(){
+
 }
 
 void wakeup()
@@ -76,6 +91,7 @@ void setup()
   Serial.begin(9600);
   delay(100);
   Serial.println();
+  Wire.begin();
 
   pinMode(12, INPUT);
 
@@ -85,11 +101,61 @@ void setup()
   Serial.printf("uuid: %s\n", getMacAddress().c_str());
   delay(100);
 
-  wakeup();
+  GPS.begin(9600,SERIAL_8N1, 16, 17);
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+  GPS.sendCommand(PGCMD_ANTENNA);
+  delay(1000);
 
-  esp_sleep_enable_ext0_wakeup(GPIO_NUM_12, 1);
-  Serial.println("Going to sleep now");
-  esp_deep_sleep_start();
+
+  //wakeup();
+
+  //esp_sleep_enable_ext0_wakeup(GPIO_NUM_12, 1);
+  //Serial.println("Going to sleep now");
+  //esp_deep_sleep_start();
 }
 
-void loop() {}
+void loop() {
+    // read data from the GPS in the 'main loop'
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if (GPSECHO)
+    if (c) Serial.print(c);
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences!
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
+      return; // we can fail to parse a sentence in which case we should just wait for another
+  }
+  // if millis() or timer wraps around, we'll just reset it
+  if (timer > millis()) timer = millis();
+     
+  // approximately every 2 seconds or so, print out the current stats
+  if (millis() - timer > 2000) {
+    timer = millis(); // reset the timer
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print(GPS.seconds, DEC); Serial.print('.');
+    Serial.println(GPS.milliseconds);
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
+    if (GPS.fix) {
+      Serial.print("Location: ");
+      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
+      Serial.print(", ");
+      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
+      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
+      Serial.print("Angle: "); Serial.println(GPS.angle);
+      Serial.print("Altitude: "); Serial.println(GPS.altitude);
+      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+    }
+  }
+}
