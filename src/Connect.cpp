820ae9decc321e56a6ee4e600c33bf4d0baf4025
Embedded-Solution <63601771+Embedded-Solution@users.noreply.github.com>
2023-04-17 22:28:58 +0200

    Revert "remove led error"
    This reverts commit d076e4318569d4f5bf4f201d677a7b53e7230431.

--- src/Connect.cpp
+++ src/Connect.cpp
@@ -292,6 +292,8 @@ void startPortal(SecureDigital sd)
                 log_e("Error after OTA");
             }
 
+            pinMode(GPIO_LED1, OUTPUT);
+            digitalWrite(GPIO_LED1, error);
             digitalWrite(GPIO_LED2, LOW);
             log_v("OTA finished, waiting for usb disconnection");
 
